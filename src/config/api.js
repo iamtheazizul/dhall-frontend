874565fa91ce7c8e2b93fa71b5d02b46312464b9
iamtheazizul <iamtheazizul@gmail.com>
2026-02-17 00:17:54 -0500

    updated api

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:8080';
-export const API_BASE_URL = 'https://dhall-backend.onrender.com';
+export const API_BASE_URL = 'https://dhallbackend-production.up.railway.app';
 
